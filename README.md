0af6be87147812b3dab839e01bbc5770906e68b7
mahaisha <mahajanisha@gmail.com>
2024-08-14 11:47:51 -0500

    Initail commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# AssuredTitan-Team7" 
---
+++ src/test/java/api/Utility/commonUtils.java
@@ -0,0 +1,18 @@
+package api.Utility;
+
+import java.util.ResourceBundle;
+
+public class commonUtils {
+
+
+	public static ResourceBundle endpoints = ResourceBundle.getBundle("endpoint");
+	
+	
+	
+	
+	
+//	public static LoginPojo loginPojo = new LoginPojo();
+//	public static PatientPojo patientPojo= new PatientPojo();
+//	public static Response response;
+//	
+}
