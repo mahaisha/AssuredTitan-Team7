9ab4f96a7582cf2d0e1b8fa88eb9a6c941fecb97
Divya Chakilam <161245630+DivyaChakilam@users.noreply.github.com>
2024-08-18 21:29:53 -0400

    Update CommonUtils.java

--- src/test/java/api/Utility/CommonUtils.java
+++ src/test/java/api/Utility/CommonUtils.java
@@ -1,45 +1,21 @@
+
 package api.Utility;
 
 import java.util.ResourceBundle;
 
-import api.Pojo.PatientPojo;
+import io.restassured.response.Response;
 
 public class CommonUtils {
 
 
 	public static ResourceBundle endpoints = ResourceBundle.getBundle("endpoint");
-	public static ResourceBundle paths = ResourceBundle.getBundle("path");
 	
-	private static String dieticianToken;
-    private static String patientToken;
-    private static String adminToken;
-
-    public static String getAdminToken() {
-		return adminToken;
-	}
-
-	public static void setAdminToken(String token) {
-		adminToken = token;
-	}
-
-	public static String getDieticianToken() {
-        return dieticianToken;
-    }
-
-    public static void setDieticianToken(String token) {
-        dieticianToken = token;
-    }
-
-    public static String getPatientToken() {
-        return patientToken;
-    }
-
-    public static void setPatientToken(String token) {
-        patientToken = token;
-    }
 	
-	//public static LoginPojo loginPojo = new LoginPojo();
-	public static PatientPojo patientPojo= new PatientPojo();
-//	public static Response response;
+	
+	
+	
+//	public static LoginPojo loginPojo = new LoginPojo();
+//	public static PatientPojo patientPojo= new PatientPojo();
+	public static Response response;
 //	
 }
