951efe2bbc13ed098195e0fa7d8991dbbe14cc6d
Divya Chakilam <161245630+DivyaChakilam@users.noreply.github.com>
2024-08-18 21:25:14 -0400

    Update createPatientPayload.java

--- src/test/java/api/Payload/createPatientPayload.java
+++ src/test/java/api/Payload/createPatientPayload.java
@@ -13,12 +13,12 @@ import api.Pojo.PatientPojo;
 import api.Utility.CommonUtils;
 import api.Utility.ExcelReader;
 
-public class createPatientPayload extends CommonUtils {
+public class CreatePatientPayload extends CommonUtils {
 	
-	//public static List<PatientPojo> createPatient() throws InvalidFormatException, IOException {
+	public static List<PatientPojo> createPatient() throws InvalidFormatException, IOException {
 		
 		 ExcelReader excelReader = new ExcelReader();
-        // List<Map<String, String>> data = excelReader.getData();
+         List<Map<String, String>> data = excelReader.getData();
 
          // Assuming there's only one record in the list, we'll use the first one.
         // Map<String, String> patientData = data.get(0);
@@ -27,26 +27,26 @@ public class createPatientPayload extends CommonUtils {
          List<PatientPojo> patients = new ArrayList<>();
 
          // Create an iterator for the list
-         //Iterator<Map<String, String>> iterator = data.iterator();
+         Iterator<Map<String, String>> iterator = data.iterator();
 
-			/*
-			 * while (iterator.hasNext()) { Map<String, String> patientData =
-			 * iterator.next();
-			 * 
-			 * 
-			 * 
-			 * patientPojo.setFirstName(patientData.get("Fname"));
-			 * patientPojo.setLastName(patientData.get("Lname"));
-			 * patientPojo.setContactNumber(patientData.get("ConatctNo"));
-			 * patientPojo.setEmail(patientData.get("email"));
-			 * patientPojo.setAllergy(patientData.get("allergy"));
-			 * patientPojo.setFoodPrefernce(patientData.get("Food"));
-			 * patientPojo.setCuisineCategory(patientData.get("cuisine"));
-			 * patientPojo.setDateOfBirth(patientData.get("DOB"));
-			 * 
-			 * // Add the populated PatientPojo object to the list
-			 * patients.add(patientPojo); }
-			 * 
-			 * return patients; // Return the list of PatientPojo objects
-			 */}
-//}
+         while (iterator.hasNext()) {
+             Map<String, String> patientData = iterator.next();
+             
+     
+         
+		patientPojo.setFirstName(patientData.get("Fname"));
+		patientPojo.setLastName(patientData.get("Lname"));
+		patientPojo.setContactNumber(patientData.get("ConatctNo"));
+		patientPojo.setEmail(patientData.get("email"));
+		patientPojo.setAllergy(patientData.get("allergy"));
+		patientPojo.setFoodPrefernce(patientData.get("Food"));
+		patientPojo.setCuisineCategory(patientData.get("cuisine"));
+		patientPojo.setDateOfBirth(patientData.get("DOB"));
+		
+		 // Add the populated PatientPojo object to the list
+        patients.add(patientPojo);
+    }
+    
+    return patients; // Return the list of PatientPojo objects
+}
+}
