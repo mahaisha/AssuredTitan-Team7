3363f61b216b63d790af2aaa6b9b2083b52647f2
Manali Shinde <manalishinde03@gmail.com>
2024-08-19 02:35:21 -0500

    Adding morbidity module scenarios

---
+++ src/test/java/api/Request/MorbidityRequest.java
@@ -0,0 +1,211 @@
+package api.Request;
+
+import api.Payload.LoginPayload;
+import api.Pojo.LoginRequestPojo;
+import api.Utility.CommonUtils;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class MorbidityRequest extends CommonUtils {
+
+	static String baseUrl;
+	private static String token;
+	public Response response;
+	static int statusCode;
+	static RequestSpecification request;
+
+	static LoginRequestPojo loginRequestPojo = new LoginRequestPojo();
+
+	public MorbidityRequest(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	public Response dieticianLoginRequest() {
+
+		loginRequestPojo = LoginPayload.dieticianLogin();
+		response = RestAssured.given()
+
+				.baseUri(baseURI).contentType(ContentType.JSON).body(loginRequestPojo).log().all()
+				.post(endpoints.getString("login"));
+
+		token = response.jsonPath().getString("token");
+		System.out.println("Token :" + token);
+		if (token == null)
+			throw new RuntimeException("Token not generated!!");
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+
+		return response;
+
+	}
+
+	public Response getAllMorbidities() {
+
+		response = getAPIUsingToken(token, endpoints.getString("morbidity"));
+
+		return response;
+	}
+
+	public Response postMorbidityInvalidMethod() {
+
+		response = RestAssured.given()
+
+				.baseUri(baseURI).contentType(ContentType.JSON).body(loginRequestPojo)
+				.header("Authorization", "Bearer " + token)
+				.log().all()
+				.post(endpoints.getString("morbidity"));
+
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+		return response;
+	}
+
+	public Response postMorbidityByNameInvalidMethod() {
+
+
+		String morbidityByName = endpoints.getString("morbidityByTestName");
+		String testName = endpoints.getString("testName");
+		String morbidityByNameEndpoint = morbidityByName.replace("{morbidityName}", testName);
+		response = RestAssured.given()
+
+				.baseUri(baseURI).contentType(ContentType.JSON).body(loginRequestPojo)
+				.header("Authorization", "Bearer " + token)
+				.log().all()
+				.post(morbidityByNameEndpoint);
+
+		System.out.println("Status Code :" + response.getStatusCode());
+		return response;
+	}
+
+	public Response getMorbidityInvalidEndpoint() {
+
+		String morbidityInvalidEndpoint = endpoints.getString("invalidEndpoint");
+
+		response = getAPIUsingToken(token, morbidityInvalidEndpoint);
+		System.out.println("Status Code :" + response.getStatusCode());
+		
+		return response;
+	}
+	
+	public Response getMorbidityByTestNameInvalidEndpoint() {
+
+		String morbiditybyTestName = endpoints.getString("morbidityByTestName");
+		String morbidityInvalidEndpoint = endpoints.getString("invalidEndpoint");
+		String morbidityByNameEndpoint = morbiditybyTestName.replace("{morbidityName}", morbidityInvalidEndpoint);
+		
+
+		response = getAPIUsingToken(token, morbidityByNameEndpoint);
+		System.out.println("Status Code :" + response.getStatusCode());
+		return response;
+	}
+	
+	public Response getMorbidityInvalidTestName() {
+
+		String morbidityInvalidTestName = endpoints.getString("invalidTestName");
+
+		response = getAPIUsingToken(token, morbidityInvalidTestName);
+		
+		return response;
+	}
+	public Response getMorbidityNoAuthToken() {
+
+		RestAssured.baseURI = baseURI;
+		request = RestAssured.given();
+
+		request.header("Content-Type", "application/json");
+		request.header("Authorization", "No Auth");
+		request.log().all();
+		response = request.get(endpoints.getString("morbidity"));
+		
+		System.out.println("Status Code :" + response.getStatusCode());
+
+		return response;
+	}
+	public Response getMorbidityTestNameNoAuthToken() {
+
+		RestAssured.baseURI = baseURI;
+		request = RestAssured.given();
+
+		request.header("Content-Type", "application/json");
+		request.header("Authorization", "No Auth");
+		String morbidityByName = endpoints.getString("morbidityByTestName");
+		String morbidityByNameEndpoint = morbidityByName.replace("{morbidityName}", "Fasting Glucose");
+		request.log().all();
+		response = request.get(morbidityByNameEndpoint);
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+
+		return response;
+	}
+
+	public Response getMorbidityByTestName() {
+
+		String morbidityByName = endpoints.getString("morbidityByTestName");
+		String testName = endpoints.getString("testName");
+		String morbidityByNameEndpoint = morbidityByName.replace("{morbidityName}", testName);
+
+		response = getAPIUsingToken(token, morbidityByNameEndpoint);
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+		return response;
+	}
+
+	public Response getAPIUsingToken(String token, String endPoint) {
+		response = RestAssured.given()
+
+				.baseUri(endpoints.getString("baseUrl")).contentType(ContentType.JSON)
+				.header("Authorization", "Bearer " + token)
+				.log().all()
+				.get(endPoint);
+
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+		return response;
+	}
+	
+	public Response patientLoginRequest() {
+
+		loginRequestPojo = LoginPayload.patientLogin();
+		response = RestAssured.given()
+
+				.baseUri(baseURI)
+				.contentType(ContentType.JSON)
+				.body(loginRequestPojo)
+				.log().all()
+				.post(endpoints.getString("login"));
+
+		token = response.jsonPath().getString("token");
+		System.out.println("Token :" + token);
+		if (token == null)
+			throw new RuntimeException("Token not generated!!");
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+
+		return response;
+	}
+	
+	public Response adminLoginRequest() {
+
+		loginRequestPojo = LoginPayload.adminLogin();
+		response = RestAssured.given()
+
+				.baseUri(baseURI)
+				.contentType(ContentType.JSON)
+				.body(loginRequestPojo)
+				.log().all()
+				.post(endpoints.getString("login"));
+
+		token = response.jsonPath().getString("token");
+		System.out.println("Token :" + token);
+		if (token == null)
+			throw new RuntimeException("Token not generated!!");
+		statusCode = response.getStatusCode();
+		System.out.println("Status Code :" + statusCode);
+
+		return response;
+
+	}
+
+}
--- src/test/resources/feature/Morbidity.feature
+++ src/test/resources/feature/Morbidity.feature
@@ -1,10 +1,108 @@
-
 @ValidateMorbidityModule
-Feature: Title of your feature
-  I want to use this template for my feature file
+Feature: Morbidity Module
 
   @TC01
   Scenario: Validate Dietician can retrieve all morbidity details
-    Given Dietician create GET request
-    When Dietician sends GET request with endpoint
-    Then Dietician receives status 401 unauthorized
+    Given User is logged in as Dietician with Token
+    And Dietician sends GET request to get morbidities
+    Then Dietician receives all morbidity details
+
+  @TC02
+  Scenario: Validate Dietician gets 405 status code for invalid method
+    Given User is logged in as Dietician with Token
+    And Dietician creates POST request for morbidity
+    Then Dietician gets 405 method not allowed
+
+  @TC03
+  Scenario: Validate Dietician gets 404 status code for invalid endpoint
+    Given User is logged in as Dietician with Token
+    And Dietician creates GET request for morbidity with invalid endpoint
+    Then Dietician should get 404 not found
+
+  @TC04
+  Scenario: Validate Dietician gets 405 status code for invalid method by Test Name
+    Given User is logged in as Dietician with Token
+    When Dietician sends POST request with morbidity by Test Name endpoint
+    Then Dietician gets 405 method not allowed
+
+  @TC05
+  Scenario: Validate Dietician can retrieve morbidity details with Test Name
+    Given User is logged in as Dietician with Token
+    And Dietician user sends GET request with endpoint having Test Name
+    Then Dietician receives morbidity details with Test Name
+
+  @TC06 @RerunForDefect
+  Scenario: Validate Dietician gets 404 status code for invalid endpoint by Test Name
+    Given User is logged in as Dietician with Token
+    And GET request for morbidity by Test Name with invalid endpoint
+    Then Dietician should get 404 not found
+
+  @TC07
+  Scenario: Validate Dietician gets 404 status code for invalid Test Name
+    Given User is logged in as Dietician with Token
+    And Dietician GET request for morbidity having invalid test name
+    Then Dietician should get 404 not found
+
+  @TC08
+  Scenario: Validate Dietician gets 401 status code for No Auth Token
+    Given Dietician sends GET request to get morbidities with NO Auth Token
+    Then Dietician receives 401 unauthorized
+
+  @TC09
+  Scenario: Validate Dietician gets 401 unauthorized for No Auth Token -morbidity By Test name
+    Given Dietician sends GET request having Test Name with NO Auth Token
+    Then Dietician receives 401 unauthorized
+
+  @TC10
+  Scenario: Validate Patient gets 403 forbidden for Morbidities with Patient Token
+    Given User is logged in as Patient with Patient Token
+    And Patient sends GET request to get morbidities
+    Then Patient receives 403 forbidden
+
+  @TC11
+  Scenario: Validate Patient gets 403 forbidden for Morbidity Test name with Patient Token
+    Given User is logged in as Patient with Patient Token
+    And Patient sends GET request to get morbidities having Test Name
+    Then Patient receives 403 forbidden
+
+  @TC12
+  Scenario: Validate Admin gets all morbidities with token
+    Given User is logged in as Admin with valid Token
+    And Admin sends GET request to get morbidities
+    Then Admin receives all morbidity details
+
+  @TC13
+  Scenario: Validate Admin gets 405 status code for invalid method
+    Given User is logged in as Admin with valid Token
+    And Admin creates POST request for morbidity
+    Then Admin gets 405 method not allowed
+    
+  @TC14
+  Scenario: Validate Admin gets 404 status code for invalid endpoint
+    Given User is logged in as Admin with valid Token
+    And Admin creates GET request for morbidity with invalid endpoint
+    Then Admin should get 404 not found
+
+   @TC15
+  Scenario: Validate Admin can retrieve morbidity details with Test Name
+    Given User is logged in as Admin with valid Token
+    And Admin user sends GET request with endpoint having Test Name
+    Then Admin receives morbidity details with Test Name
+
+  @TC16
+  Scenario: Validate Admin gets 405 status code for invalid method by Test Name
+    Given User is logged in as Admin with valid Token
+    When Admin sends POST request with morbidity by Test Name endpoint
+    Then Admin gets 405 method not allowed
+
+  @TC17
+  Scenario: Validate Admin gets 404 status code for invalid endpoint by Test Name
+    Given User is logged in as Admin with valid Token
+    And Admin GET request for morbidity by Test Name with invalid endpoint
+    Then Admin should get 404 not found
+
+  @TC18
+  Scenario: Validate Admin gets 404 status code for invalid Test Name
+    Given User is logged in as Admin with valid Token
+    And Admin GET request for morbidity having invalid test name
+    Then Admin should get 404 not found
